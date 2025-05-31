0f1dae237df6fb74fe0b94d9d379bf4d0aaf939f
Marcelo de Oliveira Carneiro <marcelo.oliveira.carneiro@gmail.com>
2025-05-31 10:02:05 -0300

    feat: add after READ Cutomer logic

--- srv/main.ts
+++ srv/main.ts
@@ -1,1 +1,12 @@
-import { } from '@models/sales';
+import { Service } from '@sap/cds';
+import { Customers } from '@models/sales'
+
+export default (service: Service) => {
+    service.after('READ','Customers', (results: Customers) => {
+        results.forEach( customer => {
+            if (!customer.email?.includes('@')) {
+                customer.email = `${customer.email}@gmail`;
+            }
+        })
+    });
+}
